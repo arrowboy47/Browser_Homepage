61c4c562482d158bce158ab8120dc087988af883
Aiden Kelly <aidenquinkelly@gmail.com>
2026-04-17 18:09:42 -0700

    my custom changes to the Habu project

--- app/js/getBadges.js
+++ app/js/getBadges.js
@@ -5,22 +5,23 @@ const WEATHER_ELEMENT = document.getElementById("weather_data");
 const TIME_ELEMENT = document.getElementById("time_data");
 const DATE_ELEMENT = document.getElementById("date_data");
 
-const handleApiUrl = (lat, long) => `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current_weather=true`;
+const handleApiUrl = (lat, long) => `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current_weather=true&temperature_unit=fahrenheit`;
 
 const getCurrentWeather = async () => {
   const dbGeo = await fetch("db/userGeo.json").then((res) => res.json());
   const apiData = await fetch(handleApiUrl(dbGeo.latitude, dbGeo.longitude)).then((res) => res.json());
 
   const weatherTemperature = Math.round(apiData.current_weather.temperature);
-  WEATHER_ELEMENT.textContent = `${weatherTemperature}°C`;
+  WEATHER_ELEMENT.textContent = `${weatherTemperature}°F`;
 };
 
 const getCurrentTime = () => {
   const date = new Date();
-  const hours = date.getHours().toString().padStart(2, "0");
-  const minutes = date.getMinutes().toString().padStart(2, "0");
-
-  TIME_ELEMENT.textContent = `${hours}:${minutes}`;
+  TIME_ELEMENT.textContent = date.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true
+  });
 };
 
 const getCurrentDate = () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 const { addDynamicIconSelectors } = require("@iconify/tailwind");
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{html,js}"],
+  content: ["./app/**/*.{html,js,json}"],
+  safelist: [
+    "icon-[ri--github-fill]",
+    "icon-[ri--spotify-fill]",
+    "icon-[simple-icons--homarr]",
+    "icon-[ri--mail-fill]",
+    "icon-[simple-icons--anthropic]",   
+    "icon-[solar--book-bold]",
+    "icon-[ph--microsoft-outlook-logo-light]",
+    "icon-[game-icons--triforce]",
+  ],
   theme: {
     extend: {},
   },
